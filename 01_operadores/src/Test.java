44c79a6de144d9e5104fa9356363b9e3a41b9e3d
antonio <antonio@gmail.com>
2018-01-09 17:32:33 +0100

    primer commit

---
+++ 01_operadores/src/Test.java
@@ -0,0 +1,16 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		/*int a=3;
+		double c=9.5, n=3.0;
+		boolean x=false;
+		System.out.println(a>c);//correcto, resultado false
+		System.out.println(a==n);//correcto, resultado false*/
+		//System.out.println(a!=x);//error de compilación
+		int a=3, b=5, c;
+		c=a>b?a*a:b--;
+
+	}
+
+}
